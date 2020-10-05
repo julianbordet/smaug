5ac05dab981232acf6a89b5086b5cea5733ceeeb
Julian <bordet.julian@gmail.com>
2020-10-05 15:47:43 -0300

    initial springboot project setup

---
+++ src/test/java/cc/jbdev/smaug/SmaugApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.jbdev.smaug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmaugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
