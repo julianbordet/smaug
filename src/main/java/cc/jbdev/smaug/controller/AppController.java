f6771a591b653fb7cb1fbe189bd44d9e3df597a5
Julian <bordet.julian@gmail.com>
2020-10-05 16:35:00 -0300

    initial home setup

---
+++ src/main/java/cc/jbdev/smaug/controller/AppController.java
@@ -0,0 +1,17 @@
+package cc.jbdev.smaug.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class AppController {
+
+    @GetMapping("/")
+    public String viewHome(){
+        return "home";
+    }
+
+
+}
