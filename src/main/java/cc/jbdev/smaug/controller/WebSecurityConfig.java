f6f9033cedaf5c8a6421da13a6979373fcc5fa91
Julian <bordet.julian@gmail.com>
2020-10-05 22:54:06 -0300

    step2

--- src/main/java/cc/jbdev/smaug/controller/WebSecurityConfig.java
+++ src/main/java/cc/jbdev/smaug/controller/WebSecurityConfig.java
@@ -18,11 +18,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
+                .antMatchers("/", "/home", "/js**", "/css/**", "/img/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
+                .loginProcessingUrl("/authenticateTheUser")
                 .permitAll()
                 .and()
                 .logout()
