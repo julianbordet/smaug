3fe62ea8b1bd7c28519f926a755ab43d4a793621
unknown <bordet.julian@gmail.com>
2021-11-05 10:23:57 -0300

    sd

--- src/main/java/cc/jbdev/smaug/controller/MvcConfig.java
+++ src/main/java/cc/jbdev/smaug/controller/MvcConfig.java
@@ -9,6 +9,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
+        registry.addViewController("/login").setViewName("login");
     }
 }
