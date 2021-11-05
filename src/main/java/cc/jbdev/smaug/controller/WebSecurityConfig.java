6c8c9699a5e30898909fa014f20fa03533b230b2
unknown <bordet.julian@gmail.com>
2021-11-04 21:50:10 -0300

    asd

--- src/main/java/cc/jbdev/smaug/controller/WebSecurityConfig.java
+++ src/main/java/cc/jbdev/smaug/controller/WebSecurityConfig.java
@@ -29,7 +29,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/js**", "/css/**", "/img/**").permitAll()
+                .antMatchers("/", "/home", "/js**", "/css/**", "/img/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
